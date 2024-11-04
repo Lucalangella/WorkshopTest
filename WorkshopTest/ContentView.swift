f62a3fd8a40fea3c8e4631adb44b5535071ec8d3
Lucalangella <langellaluca00@gmail.com>
2024-11-04 10:34:15 +0100

    Initial Commit

---
+++ WorkshopTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkshopTest
+//
+//  Created by Luca Langella 1 on 04/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkshopTest/WorkshopTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopTestApp.swift
+//  WorkshopTest
+//
+//  Created by Luca Langella 1 on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
